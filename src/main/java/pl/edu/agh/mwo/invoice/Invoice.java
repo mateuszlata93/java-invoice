dfbffac5e6df22dc8a0a2c2b1ba8c02a205687ca
mlata_wh <mateusz.lata@grandparade.co.uk>
2023-02-13 21:05:17 +0100

    fix invoice tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,41 +1,46 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products = new ArrayList<>();
-    //private Map<Product, Integer> products = new HashMap<>();
+    private List<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
+        if (product == null) throw new IllegalArgumentException("product should not be null");
         this.products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        for (int i =0; i<quantity; i++){
+        if (quantity <= 0) throw new IllegalArgumentException("quantity should not be negative");
+        for (int i = 0; i < quantity; i++) {
             this.products.add(product);
         }
     }
 
     public BigDecimal getSubtotal() {
-        if(products == null) return BigDecimal.ZERO;
+        if (products == null) return BigDecimal.ZERO;
         return this.products.stream()
                 .map(product -> product.getPrice())
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
-        };
+    }
+
 
     public BigDecimal getTax() {
-        if(products == null) return BigDecimal.ZERO;
-        return null;
+        if (products == null) return BigDecimal.ZERO;
+        //return null;
+        BigDecimal taxTotal = products.stream()
+                .map(product -> product.getPriceWithTax().subtract(product.getPrice()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return taxTotal;
     }
 
     public BigDecimal getTotal() {
-        if(products == null) return BigDecimal.ZERO;
-        return null;
+        if (products == null) return BigDecimal.ZERO;
+        return products.stream()
+                .map(product -> product.getPriceWithTax())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
